940c31ac2cc8aa3db5e3c539f67dd1506df6a9b1
Jeevan Joshi <117514522+JeevanJoshi4434@users.noreply.github.com>
2024-12-15 14:34:20 +0530

    Update server.js for port env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware to parse incoming JSON data
 app.use(bodyParser.json());
